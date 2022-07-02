acf5e41e54e427a48332212223bc0acdfde84718
DESKTOP-KL55BTV\0800f <0800fer@gmail.com>
2022-07-02 19:41:24 +0200

    Definición de la entidad PODER

---
+++ src/main/java/com/proyecto/app/entidad/Poder.java
@@ -0,0 +1,40 @@
+package com.proyecto.app.entidad;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Definición de la entidad que representa un PODER
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "poderes")
+public class Poder {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+
+	@Column(name = "nombre")
+	@NotNull
+	@Size(max = 50)
+	private String nombre;
+
+	@Column(name = "descripcion")
+	@Size(max = 256)
+	private String descripcion;
+
+}
