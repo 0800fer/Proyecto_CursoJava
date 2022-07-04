4dc88c662645d754b0cf1cf5ba087ea536c84954
DESKTOP-KL55BTV\0800f <0800fer@gmail.com>
2022-07-04 23:35:28 +0200

    Creación del repositorio Universo

--- src/main/java/com/proyecto/app/entidades/UniversoDTO.java
+++ src/main/java/com/proyecto/app/entidades/UniversoDTO.java
@@ -1,8 +1,12 @@
 package com.proyecto.app.entidades;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UniversoDTO {
 
 	private Integer id;
---
+++ src/test/java/com/proyecto/app/entidades/PoderModelMapperTest.java
@@ -0,0 +1,36 @@
+package com.proyecto.app.entidades;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
+
+class PoderModelMapperTest {
+
+	private ModelMapper modelMapper = new ModelMapper();
+
+	@Test
+	@DisplayName("Test mapper DTO a entidad")
+	void dtoToEntidadTest() {
+		PoderDTO dto = new PoderDTO(123, "Veloz", "Descripcion");
+
+		Poder poder = modelMapper.map(dto, Poder.class);
+
+		assertThat(poder.getId()).isEqualTo(dto.getId());
+		assertThat(poder.getNombre()).isEqualTo(dto.getNombre());
+		assertThat(poder.getDescripcion()).isEqualTo(dto.getDescripcion());
+	}
+
+	@Test
+	@DisplayName("Test mapper entidad a DTO")
+	void entidadToDtoTest() {
+		Poder poder = new Poder(123, "Volar", "Descripcion");
+
+		PoderDTO dto = modelMapper.map(poder, PoderDTO.class);
+
+		assertThat(dto.getId()).isEqualTo(poder.getId());
+		assertThat(dto.getNombre()).isEqualTo(poder.getNombre());
+		assertThat(dto.getDescripcion()).isEqualTo(poder.getDescripcion());
+	}
+}
